04a1262674335071d8fb9a3871c17df2dd051e37
Arlinda <arlindaputri273@gmail.com>
2023-06-19 13:16:24 +0700

    Update Constant

---
+++ lib/util/Constant.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static const String defaultProfilePhotoUrl = "https://gravatar.com/avatar";
+
+  // Padding
+  static const double kPaddingXS = 4.0;
+  static const double kPaddingS = 8.0;
+  static const double kPaddingM = 14.0;
+  static const double kPaddingL = 16.0;
+  static const double kPaddingXL = 32.0;
+
+  // Font Size
+  static const double kFontSizeXS = 6.0;
+  static const double kFontSizeS = 12.0;
+  static const double kFontSizeM = 14.0;
+  static const double kFontSizeL = 16.0;
+  static const double kFontSizeXL = 18.0;
+  static const double kFontSizeXXL = 20.0;
+  static const double kFontSizeX = 24.0;
+  static const double kFontSizeXX = 32.0;
+
+  // Font Weight
+  static const FontWeight kFontWeightS = FontWeight.w400;
+  static const FontWeight kFontWeightM = FontWeight.w500;
+  static const FontWeight kFontWeightL = FontWeight.w600;
+  static const FontWeight kFontWeightXL = FontWeight.w700;
+  static const FontWeight kFontWeightX = FontWeight.w800;
+
+  // Radius
+  static const double kRadiusS = 4.0;
+  static const double kRadiusM = 8.0;
+  static const double kRadiusL = 14.0;
+  static const double kRadiusXL = 32.0;
+
+  // Animation
+  static const Duration kPageAnimationDuration = Duration(milliseconds: 375);
+  static const Duration kButtonAnimationDuration = Duration(milliseconds: 600);
+  static const Duration kCardAnimationDuration = Duration(milliseconds: 400);
+  static const Duration kRippleAnimationDuration = Duration(milliseconds: 400);
+}
